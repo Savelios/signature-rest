bf153ca87befc935a571a7c6f2ccca0a953c20d3
Savelios Stefanidis <97344586+id000000369@users.noreply.github.com>
2023-11-17 19:00:40 +0300

    add

--- src/App.tsx
+++ src/App.tsx
@@ -65,7 +65,7 @@ function App() {
     if (location.pathname === "/pictures" && windowSize.current[0] > 0) {
       footer[0].setAttribute("style", "display:none;");
     } else {
-      footer[0].setAttribute("style", "display:flex;");
+      footer[0].setAttribute("style", "display:none;");
     }
   }, [location]);
 
