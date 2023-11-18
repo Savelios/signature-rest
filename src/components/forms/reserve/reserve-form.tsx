d86e48a1e1df83fff3840919a26bbf4637edaf8c
Savelios Stefanidis <97344586+id000000369@users.noreply.github.com>
2023-11-18 15:00:48 +0300

    add

--- src/components/forms/reserve/reserve-form.tsx
+++ src/components/forms/reserve/reserve-form.tsx
@@ -1,11 +1,10 @@
 import React, { ReactNode } from "react";
-import "./reserve-form.css";
-// import closeFormBtn from "../../../assets/close-form-btn.png"
-// import closeFormBtnMob from "../../../assets/close-modal-btn.png"
-// import layoutImg from '../../../assets/reserve-form-layout.png'
-// import reserveLogo from "../../../assets/gold-logo.png"
+import closeFormBtn from "../../../assets/close-form-btn.png"
+import closeFormBtnMob from "../../../assets/close-modal-btn.png"
+import reserveLogo from "../../../assets/gold-logo.png"
 import { CheckBox } from "../../../ui/checkbox/checkbox";
 
+import "./reserve-form.css";
 
 type ModalProps = {
   onCloseModal: () => void;
@@ -14,75 +13,69 @@ type ModalProps = {
 const ReserveForm: React.FC<ModalProps> = ({ onCloseModal }) => {
   return (
     <>
-      <div className="main-reserve-container"  >
-        <div className="reserve-container">
-          {/* <div className="reserve-vertical-line"></div> */}
-          <div className="close-from-container">
-            <img className="reserve-close-form-button" src={"closeFormBtn"} alt="" onClick={onCloseModal} />
-            <img className="reserve-close-form-button-mob" src={"closeFormBtnMob"} alt="" onClick={onCloseModal} />
-          </div>
-          <img className="reserve-logo" src={"reserveLogo"} alt="" />
-          <h2 className="reserve-title">ЗАБРОНИРОВАТЬ<p className="reserve-title-last-word">СТОЛИК </p></h2>
-          <div className="input-items-main-container">
-            <div className="input-items-container">
-              <label className="name-prefix-label" htmlFor="uname">
-                ПРЕДСТАВЬТЕСЬ ПОЖАЛУЙСТА
-              </label>
-              <input
-                type="text"
-                id="uname"
-                placeholder="Виктория"
-                className="name-input"
-                required
-                minLength={2}
-                maxLength={18}
-              ></input>
-              <label className="phone-prefix-label" htmlFor="phone">
-                ТЕЛЕФОН
-              </label>
-              <input
-                type="text"
-                id="phone"
-                placeholder="+7 ( - - - ) - - -  - -  - -"
-                className="phone-input"
-                required
-                minLength={11}
-                maxLength={11}
-              ></input>
-              <label className="date-prefix-label" htmlFor="date">
-                ДАТА
-              </label>
-              <input
-                type="datetime-local"
-                id="date"
-                className="date-input"
-                placeholder="18 апреля 2023  18:00"
-                required
-              ></input>
-              <label className="guest-prefix-label" htmlFor="guest">
-                НАС БУДЕТ
-              </label>
-              <input
-                type="guest"
-                id="guest"
-                className="guest-input"
-                placeholder="4 гостя"
-                required
-              ></input>
+      <div className="reserve__container"  >
+        <div className="reserve__form">
+          <img className="reserve-form__close-btn" src={closeFormBtn} alt="" onClick={onCloseModal} />
+          {/* <img className="reserve-close-form-button-mob" src={"closeFormBtnMob"} alt="" onClick={onCloseModal} /> */}
+          <img className="reserve-form__logo" src={"reserveLogo"} alt="" />
+          <h2 className="reserve-form__title">ЗАБРОНИРОВАТЬ<span>СТОЛИК</span></h2>
+          <div className="reserve-form__input-container">
+            {/* <div className="reserve-form__separator"></div> */}
 
-              <button className="reserve-button">Забронировать столик</button>
-              <div className="bottom-container">
-                <div className="reminder-container">
-                  <p className="reminder-text">Напомнить мне о бронировании</p>
-                  <CheckBox />
-                </div>
-                <p className="reminder-info-text">
-                  Вам придет пуш-уведомление о бронировании, чтобы мы с вами точно
-                  встретились :)
-                </p>
-              </div>
-            </div>
+            <label className="reserve__from_prefix-label" htmlFor="reserve_uname-id">
+              ПРЕДСТАВЬТЕСЬ ПОЖАЛУЙСТА
+            </label>
+            <input
+              type="text"
+              id="reserve_uname-id"
+              placeholder="Виктория"
+              className="reserve__form_input-value"
+              required
+              minLength={2}
+              maxLength={18}
+            ></input>
+            <label className="reserve__from_prefix-label" htmlFor="reserve_phone-id">
+              ТЕЛЕФОН
+            </label>
+            <input
+              type="text"
+              id="reserve_phone-id"
+              placeholder="+7 ( - - - ) - - -  - -  - -"
+              className="reserve__form_input-value"
+              required
+              minLength={11}
+              maxLength={11}
+            ></input>
+            <label className="reserve__from_prefix-label" htmlFor="reserve__date-id">
+              ДАТА
+            </label>
+            <input
+              type="datetime-local"
+              id="reserve__date-id"
+              className="reserve__form_input-value"
+              placeholder="18 апреля 2023  18:00"
+              required
+            ></input>
+            <label className="reserve__from_prefix-label" htmlFor="reserve__guest-id">
+              НАС БУДЕТ
+            </label>
+            <input
+              type="guest"
+              id="reserve__guest-id"
+              className="reserve__form_input-value"
+              placeholder="4 гостя"
+              required
+            ></input>
           </div>
+          <button className="reserve__form-button">Забронировать столик</button>
+          <div className="reserve__form_reminder-container">
+            <p>Напомнить мне о бронировании</p>
+            <CheckBox />
+          </div>
+          <p className="reserve__form_reminder-text">
+            Вам придет пуш-уведомление о бронировании, чтобы мы с вами точно
+            встретились :)
+          </p>
         </div>
       </div>
     </>
