dae71c7ff9cb3909170f47907fd5200ac416a6ca
Savelios Stefanidis <97344586+id000000369@users.noreply.github.com>
2023-12-01 11:34:13 +0300

    add

--- src/pages/About/AboutPage.tsx
+++ src/pages/About/AboutPage.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react'
-import { AboutIndicators } from '../../ui/indicator/about/about-indicator'
-import { AboutView } from '../../components/views/about/AboutView'
-import ReserveForm from '../../components/forms/reserve/reserve-form';
+import React, { useState } from "react";
+import { AboutIndicators } from "../../ui/indicator/about/about-indicator";
+import { AboutView } from "../../components/views/about/AboutView";
+import ReserveForm from "../../components/forms/reserve/reserve-form";
 
-
+import "./AboutPage.css"
 
 const AboutPage = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -16,10 +16,11 @@ const AboutPage = () => {
   };
   return (
     <>
+      <h1 className="about-page__title">О НАС</h1>
       <AboutIndicators />
       <AboutView onOpenModal={handleOpenModal} />
       {isModalOpen && <ReserveForm onCloseModal={handleCloseModal} />}
     </>
-  )
-}
-export default AboutPage
+  );
+};
+export default AboutPage;
