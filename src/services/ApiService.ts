d862edf178dcca1886b36b89fa147e57981293af
Savelios Stefanidis <97344586+id000000369@users.noreply.github.com>
2023-12-04 15:06:47 +0300

    add

--- src/services/ApiService.ts
+++ src/services/ApiService.ts
@@ -4,7 +4,7 @@
 import { Axios } from "axios";
 
 //Mock URL
-const API_BASE_URL: string = "http://localhost:8080/api/v1/";
+const API_BASE_URL: string = "http://localhost:8080/";
 
 // const API_BASE_URL: string = "http://89.147.109.197:8080/";
 
